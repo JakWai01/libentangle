f69f34936b3014bbff0e210d2d23ce639a048c1d
JakWai01 <jakob.waibel@web.de>
2021-10-31 11:21:51 +0100

    feat: Inject functions

--- cmd/signal.go
+++ cmd/signal.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 
+	api "github.com/alphahorizon/libentangle/pkg/api/websockets/v1"
 	"github.com/alphahorizon/libentangle/pkg/signaling"
 	"github.com/spf13/cobra"
 	"nhooyr.io/websocket"
@@ -15,7 +16,6 @@ var signalCmd = &cobra.Command{
 	Short: "Start a signaling server",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Handle lifecycle
-		signaler := signaling.NewSignalingServer()
 
 		for {
 			addr, err := net.ResolveTCPAddr("tcp", "localhost:9090")
@@ -24,6 +24,29 @@ var signalCmd = &cobra.Command{
 			}
 
 			log.Printf("signaling server listening on %v", addr)
+
+			communities := signaling.NewCommunitiesManager()
+
+			signaler := signaling.NewSignalingServer(
+				func(application api.Application, conn *websocket.Conn) error {
+					return communities.HandleApplication(application, conn)
+				},
+				func(ready api.Ready, conn *websocket.Conn) error {
+					return communities.HandleReady(ready, conn)
+				},
+				func(offer api.Offer) error {
+					return communities.HandleOffer(offer)
+				},
+				func(answer api.Answer) error {
+					return communities.HandleAnswer(answer)
+				},
+				func(candidate api.Candidate) error {
+					return communities.HandleCandidate(candidate)
+				},
+				func(exited api.Exited) error {
+					return communities.HandleExited(exited)
+				},
+			)
 
 			handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 				conn, err := websocket.Accept(rw, r, &websocket.AcceptOptions{

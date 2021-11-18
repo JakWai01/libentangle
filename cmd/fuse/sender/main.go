c16e6e2a8bf8bf77bd4abb9fcc12db082c574145
JakWai01 <jakob.waibel@web.de>
2021-11-18 19:44:50 +0100

    feat: Get all data from top level of directory

--- cmd/fuse/sender/main.go
+++ cmd/fuse/sender/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 type File struct {
-	name    string
-	content []byte
+	Name    string `json:name`
+	Content []byte `json:content`
 }
 
 type Folder struct {
-	name string
+	Name string `json:name`
 }
 
 func main() {
@@ -22,5 +24,34 @@ func main() {
 	}
 	for _, f := range files {
 		fmt.Println(f.Name())
+
+		// If File, open it, create File, send it
+		// If Folder, create Folder, send it
+
+		if f.IsDir() {
+			folder := Folder{Name: f.Name()}
+			b, err := json.Marshal(folder)
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println(string(b))
+
+			// send json to receiver
+		} else {
+			// Get this path from somewhere else
+			dat, err := os.ReadFile(fmt.Sprintf("../example/%s", f.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			file := File{Name: f.Name(), Content: dat}
+
+			b, err := json.Marshal(file)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(b))
+		}
+
 	}
 }

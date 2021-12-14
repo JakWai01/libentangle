2619673cd72df1207ebc44cdfe962c75a89128f1
JakWai01 <jakob.waibel@web.de>
2021-12-14 22:20:51 +0100

    feat: Implement reader conceptually

--- cmd/client.go
+++ cmd/client.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -19,8 +22,48 @@ var clientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "Start a signaling client.",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// We can't override a file, since it would just get appended
+		// We should replace a file if it did exist before
+		networking.Connect("test", func(msg webrtc.DataChannelMessage) {
+			// This function gets called on every message
+			log.Printf("Message: %s", msg.Data)
 
-		networking.Connect("test", func(msg webrtc.DataChannelMessage) { log.Printf("Message: %s", msg.Data) })
+			var f *os.File
+			var err error
+
+			type message struct {
+				Name    string `json:"name"`
+				Content []byte `json:"content"`
+			}
+
+			var file message
+
+			if err = json.Unmarshal(msg.Data, &file); err != nil {
+				panic(err)
+			}
+
+			// Create file
+			fmt.Println("We entered the function")
+			f, err = os.OpenFile(file.Name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			// change this to switch
+			if bytes.Equal(file.Content, []byte("BOF")) {
+				f.Truncate(0)
+				f.Seek(0, 0)
+			} else if bytes.Equal(file.Content, []byte("EOF")) {
+				return
+			} else {
+				_, err := f.Write(file.Content)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+		})
 
 		for {
 			reader := bufio.NewReader(os.Stdin)

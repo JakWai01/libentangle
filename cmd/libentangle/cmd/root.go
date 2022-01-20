9236af3beb4e1cc5a4661df6e958c96b0d30831c
JakWai01 <jakob.waibel@web.de>
2022-01-20 20:21:48 +0100

    refactor: Rename entangle cmd to libentangle

--- cmd/libentangle/cmd/root.go
+++ cmd/libentangle/cmd/root.go
@@ -18,7 +18,7 @@ const (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "entangle",
+	Use:   "libentangle",
 	Short: "A library for building peer-to-peer file sharing solutions.",
 	Long: `A library for building peer-to-peer file sharing solutions.
 	For more information, please visit https://github.com/alphahorizon/libentangle`,
